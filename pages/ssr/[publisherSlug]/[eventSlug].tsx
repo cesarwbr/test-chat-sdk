999bf1812dbc2a460e2366608e3edc0d019a5619
Cesar William <cesarwbr@gmail.com>
2021-04-28 15:43:52 -0300

    add ssr css

--- pages/ssr/[publisherSlug]/[eventSlug].tsx
+++ pages/ssr/[publisherSlug]/[eventSlug].tsx
@@ -2,21 +2,40 @@ import { FC } from "react";
 import { GetServerSideProps } from "next";
 
 const SSR: FC<{ html: string }> = ({ html }) => {
-  return <div dangerouslySetInnerHTML={{ __html: html }} />;
-};
+  return (
+    <div>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+      <style global jsx>{`
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: "Austin News Text Roman", Georgia, Times, serif;
+        }
 
-interface Cache {
-  [key: string]: {
-    html: string;
-    timestamp: number;
-  };
-}
+        blockquote {
+          background-image: url(https://www.telegraph.co.uk/etc/designs/telegraph/core/clientlibs/tmgchannels/_base/img/borders/comment__news.png);
+          line-height: 25px;
+          background-position: -1088px 0;
+          background-repeat: repeat-y;
+          color: #333;
+          font-family: "Austin News Text Roman", Georgia, Times, serif;
+          margin: 0 0 1.5rem;
+          padding: 0 2.5rem 0 2.6rem;
+        }
+
+        .summary--container {
+          padding-bottom: 0 !important;
+        }
+      `}</style>
+    </div>
+  );
+};
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { publisherSlug, eventSlug } = context.params;
 
   const data = await fetch(
-    `https://sandbox.arena.im/ssr/${context.params.publisherSlug}/${context.params.eventSlug}`,
+    `https://sandbox.arena.im/ssr/${publisherSlug}/${eventSlug}`,
     {
       headers: {
         "user-agent": context.req.headers["user-agent"],
